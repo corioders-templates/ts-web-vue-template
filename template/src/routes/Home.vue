65c70f8d2c275c763682a4f9b5a111ce758a5e35
artalatarta <artur@mucowski.pl>
2022-02-15 08:38:44 +0100

    Add better frontend

--- template/src/routes/Home.vue
+++ template/src/routes/Home.vue
@@ -1,4 +1,16 @@
 <template>
-	<p>Welcome home!</p>
-	<router-link to="/async">To AsyncComponent!</router-link>
+	<h1>Welcome home!</h1>
+	<VButton @click="$router.push('/async')">To AsyncComponent!</VButton>
 </template>
+
+<script lang="ts">
+import { VButton } from '@corioders/vueui';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+	name: 'Home',
+	components: {
+		VButton,
+	},
+});
+</script>
